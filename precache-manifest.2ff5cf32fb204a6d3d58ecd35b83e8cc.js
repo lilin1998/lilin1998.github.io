464470ce885e9f51dfaea4621a3b415266f7a522
lilin1998 <lilin1998@gmail.com>
2020-06-14 22:54:30 +0800

    Updates

---
+++ precache-manifest.2ff5cf32fb204a6d3d58ecd35b83e8cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ebb1abb82f313f17217075c6641097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f9676f2c3b0fc1af0e",
+    "url": "/static/css/2.f317a975.chunk.css"
+  },
+  {
+    "revision": "d9e59eeebe46e425ad14",
+    "url": "/static/css/main.72b79b6d.chunk.css"
+  },
+  {
+    "revision": "23f9676f2c3b0fc1af0e",
+    "url": "/static/js/2.13dabfcf.chunk.js"
+  },
+  {
+    "revision": "d9e59eeebe46e425ad14",
+    "url": "/static/js/main.581e430f.chunk.js"
+  },
+  {
+    "revision": "a0d00677a82276c1d1c6",
+    "url": "/static/js/runtime~main.95b91239.js"
+  },
+  {
+    "revision": "faedd492ea4989a51e5d499a1e660140",
+    "url": "/static/media/about-img.faedd492.jpg"
+  },
+  {
+    "revision": "34f2787505138289319a3cc6d0821b64",
+    "url": "/static/media/beach-mobile.34f27875.jpg"
+  },
+  {
+    "revision": "ae7f301e5f061c1eb060c28b36fb53a2",
+    "url": "/static/media/beach-tablet.ae7f301e.jpg"
+  },
+  {
+    "revision": "5b9a915ec53dd8d115a83c26c701b37c",
+    "url": "/static/media/beach.5b9a915e.jpg"
+  },
+  {
+    "revision": "8a97936ea4fe64e0e30199185f427d74",
+    "url": "/static/media/resume.8a97936e.jpg"
+  }
+]);
--- src/playground/es6-let-const.js
+++
@@ -1,7 +0,0 @@
-var nameVar = 'Li Lin';
-nameVar = 'Shit';
-console.log('nameVar', nameVar);
-
-let nameLet = 'Poo';
-nameLet = 'Poop';
-console.log('nameLet', nameLet);
