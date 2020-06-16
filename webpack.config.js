c1bfbfb12d65f05b554494d6af2788e88edc8e36
lilin1998 <lilin1998@gmail.com>
2020-06-16 18:57:00 +0800

    build file update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
     return {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'public', 'dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     module: {
